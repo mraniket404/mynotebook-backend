2b66da132b2e42916568bd7bc069d0d2b9a9e7cf
mraniket404 <aniketgosavi471@gmail.com>
2025-09-26 16:14:42 +0530

    backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+
+dotenv.config(); 
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
