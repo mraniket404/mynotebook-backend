2277684ebb06580aeadfb39aeb6299479070f159
mraniket404 <aniketgosavi471@gmail.com>
2025-09-30 23:42:29 +0530

    add express validation

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
